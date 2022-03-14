881a84ad94193cc41936b3c181bc90cb05f3a66d
metigator <metigator@gmail.com>
2022-03-13 22:12:23 -0400

    initial commit

---
+++ CSHARPTut040.Foreach/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CHARPTut040.Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            var numbers = new [] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            Console.WriteLine("\n\n Using For");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($" {numbers[i]}");
+            }
+
+
+            Console.WriteLine("\n\n Using Foreach");
+            foreach (var n in numbers)
+            {
+                Console.Write($" {n}");
+            }
+
+
+            Console.WriteLine("\n\n Using Foreach under the hood");
+            Foreach(numbers);
+
+
+            Console.ReadKey();
+        } 
+
+        static void Foreach<T> (IEnumerable<T> source)
+        {
+            IEnumerator<T> enumerator = source.GetEnumerator();
+            IDisposable disposable;
+            try
+            {
+                T item;
+                while (enumerator.MoveNext())
+                {
+                    // code
+                    item = enumerator.Current;
+                    Console.Write($" {item}");
+                }
+            }
+            finally
+            {
+                disposable = (IDisposable)enumerator;
+                disposable.Dispose();
+            }
+         
+
+        }
+    }
+}
---
+++ CSHARPTut040.Yield/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSHARPTut040.Yield
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("using GenerateV1()");
+            //foreach (var n in GenerateV1())
+            //{
+            //    Console.Write($" {n}");
+            //}
+
+            //Console.WriteLine("\n\nusing GenerateV2()");
+            //foreach (var n in GenerateV2())
+            //{
+            //    Console.Write($" {n}");
+            //}
+
+            Console.WriteLine("\n\nusing GenerateV3()");
+            foreach (var n in GenerateV3())
+            {
+                Console.Write($" {n}");
+            }
+            Console.ReadKey();
+        }   
+
+        static IEnumerable<int> GenerateV1()
+        {
+            var result = new List<int>();
+            for (int i = 1; i <= 5; i++)
+            {
+                result.Add(i);
+            }
+            return result;
+        }
+
+        static IEnumerable<int> GenerateV2()
+        { 
+            for (int i = 1; i <= 5; i++)
+            {
+                yield return i;
+            } 
+        }
+
+        static IEnumerable<int> GenerateV3()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
+            yield break;
+            yield return 6; 
+            yield return 7;
+            yield return 8;
+        }
+    }
+}
